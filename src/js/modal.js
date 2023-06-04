8cfd3cd825c7f77b99e12eb7c12eb5f605c1d028
Dag <dagmara.szadkowska@wp.pl>
2023-06-04 14:24:58 +0200

    Modal fixes/header

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,28 +4,27 @@
     closeModalBtn: document.querySelector('[data-modal-subscribe-close]'),
     modal: document.querySelector('[data-modal-subscribe]'),
   };
-
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  for (let i = 0; i < refs.closeModalBtn.length; i++) {
+    refs.closeModalBtn[i].addEventListener('click', toggleModal);
+  }
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
-
 (() => {
   const RefsContact = {
     openModalBtns: document.querySelectorAll('[data-modal-contact-open]'),
-    closeModalBtn: document.querySelector('[data-modal-contact-close]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-contact-close]'),
     modal: document.querySelector('[data-modal-contact]'),
   };
-  for (let i = 0; i < RefsContact.openModalBtns.length; i++) {
+  for (let i = 0; i < RefsContact.closeModalBtn.length; i++) {
     RefsContact.openModalBtns[i].addEventListener('click', toggleModal);
   }
-  console.log(RefsContact.closeModalBtn);
-  RefsContact.closeModalBtn.addEventListener('click', toggleModal);
+  for (let i = 0; i < RefsContact.closeModalBtn.length; i++) {
+    RefsContact.closeModalBtn[i].addEventListener('click', toggleModal);
+  }
   function toggleModal() {
     RefsContact.modal.classList.toggle('is-hidden');
-    console.log('click');
   }
 })();
